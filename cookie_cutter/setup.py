70101098000fccc1b251808bc92b7155b6ca197f
Lindsey Robertson <lnrobertson1@gmail.com>
2022-08-01 17:28:02 -0700

    add template folder

---
+++ cookie_cutter/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hotel data will provide insight into customer segments and lifetime value.',
+    author='Lindsey Robertson',
+    license='',
+)
